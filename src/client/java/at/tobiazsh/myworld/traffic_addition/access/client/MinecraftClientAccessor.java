81527d0f946e2be94955b4cd36d913e9665663fd
Tobias <tobias.schweighofer0809@gmail.com>
2025-07-13 14:15:09 +0200

    Remove unused import statement

--- src/client/java/at/tobiazsh/myworld/traffic_addition/access/client/MinecraftClientAccessor.java
+++ src/client/java/at/tobiazsh/myworld/traffic_addition/access/client/MinecraftClientAccessor.java
@@ -1,7 +1,6 @@
 package at.tobiazsh.myworld.traffic_addition.access.client;
 
 import net.minecraft.client.font.FontManager;
-import net.minecraft.client.option.GameOptions;
 
 public interface MinecraftClientAccessor {
     FontManager myworldTrafficAddition$getFontManager();
