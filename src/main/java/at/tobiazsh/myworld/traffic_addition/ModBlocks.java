61106d2674ef5161815ae7ea7d38b9531eea9ba2
Tobias <tobias.schweighofer0809@gmail.com>
2025-07-18 12:52:53 +0200

    Remove unused imports

--- src/main/java/at/tobiazsh/myworld/traffic_addition/ModBlocks.java
+++ src/main/java/at/tobiazsh/myworld/traffic_addition/ModBlocks.java
@@ -6,12 +6,6 @@ import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.registry.RegistryKey;
-import net.minecraft.registry.RegistryKeys;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
